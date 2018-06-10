2a18eb750be5f3ad25878beffedf93a83751ac4c
Dino Dunvjak <dinoduvnjak@gmail.com>
2018-06-10 08:26:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ var playlist = { artistName: 'songTitle'};
 function updatePlaylist(obj, key, value) {
   return obj[key] = value;
 }
+
+function removeFromPlaylist(obj,artistName) {
+  delete obj.artistName;
+  return obj
+}
